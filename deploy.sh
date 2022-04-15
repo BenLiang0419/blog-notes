e72ad90bc276588b1f4a7a692b9ad9b35aa54f0c
liangjs <asd153200>
2022-04-15 16:52:52 +0800

    feat: 提交deploy

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
  
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f https://github.com/JexLau/blog master:gh-pages
+git push -f https://github.com/BenLiang0419/something-someblog master:gh-pages
  
 cd -
